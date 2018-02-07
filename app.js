6f7eb52a2d63f72942aa13fb2a9ffef9ca653b59
cocacolar <yysun2000@nate.com>
2018-02-07 14:12:23 +0900

    구조 변경(퍼페티어)

--- app.js
+++ app.js
@@ -1,60 +1,15 @@
-const login = require('./module/puppeteer/login.js');
-const hmall = require('./module/puppeteer/hmall.js');
+const pageAuto = require('./module/puppeteer/index.js');
 const logger = require('./module/util/logger.js');
-const dm = require('./module/util/datemanager.js');
 const path = require('path');
-const DataManager = dm.DateManager;
 
 const fs = require('fs');
 
-const puppeteer = require('puppeteer');
-const USERINFO = {
-  id:"ID",
-  password:"PASSWORD",
-  credit : {
-    password : "PASSWORD"
-  }
-}
-function puppeteerRun(){
-
-  puppeteer.launch({
-    headless:false,
-    args: ['--no-sandbox', '--disable-setuid-sandbox','--enable-file-cookies']
-  }).then(async browser => {
-    var page = await browser.newPage();
-    var date = new DataManager();
-    var datapath;
-    datapath = "./hmall/"+date.text+"/";
-    await fs.mkdirSync(datapath,0777);
-    await fs.mkdirSync(datapath+"/pc/",0777);
-    await fs.mkdirSync(datapath+"/mobile/",0777);
-    await page.setViewport({width:1200,height:800});
-
-    await page.goto('http://www.hyundaihmall.com/Home.html',{waitUntil:"domcontentloaded"});
-    await page.once('load',async ()=>{
-      await page.screenshot({path:datapath+"/pc/"+"site.png",fullPage:true});
-      await page.evaluate(()=>{
-        openLoginPopup()
-      })
-
-      var param = {
-        "browser" : browser,
-        "page" : page,
-        "data" : USERINFO,
-        "folder" : date.text,
-        "datapath" : datapath+"/pc/"
-      }
-      logger.debug("INIT :: 로그인을 시작합니다.");
-      await login.run(param, hmall.main);
-    })
-  });
-}
 
 const express = require('express');
 const app = express();
 app.use(express.static('.'));
 app.listen(9642,(req,res)=>{
-  puppeteerRun()
+  pageAuto.run()
   console.log("DONE!");
 });
 app.get("/",(req,res)=>{
@@ -66,6 +21,12 @@ app.get("/",(req,res)=>{
 })
 app.get("/hmall/:date/:hour",(req,res)=>{
   var result = "";
+  /* 1. CHECK */
+  if(req.params.date == "" || req.params.hour == ""){
+    res.send("Invalid Parameter");
+    return;
+  }
+  /* 2. RUN */
   fs.readdirSync("./hmall/").forEach(file => {
     var datetime = file;
     var date = file.substr(0,8)*1;
---
+++ module/puppeteer/index.js
@@ -0,0 +1,47 @@
+const puppeteer = require('puppeteer');
+
+const login = require('./login.js');
+const hmall = require('./hmall.js');
+
+//Util
+const dmr = require('./../util/datemanager.js');
+const fs = require('fs');
+const logger = require('./../util/logger.js');
+
+const USERINFO = require('./../../userinfo.js').data;
+
+exports.run = function(){
+  puppeteer.launch({
+    headless:false,
+    args: ['--no-sandbox', '--disable-setuid-sandbox','--enable-file-cookies']
+  }).then(async browser => {
+    var page = await browser.newPage();
+    console.log(dmr);
+    var DateManager = dmr.DateManager;
+    var date = new DateManager();
+    var datapath;
+    datapath = "./hmall/"+date.text+"/";
+    await fs.mkdirSync(datapath,0777);
+    await fs.mkdirSync(datapath+"/pc/",0777);
+    await fs.mkdirSync(datapath+"/mobile/",0777);
+    await page.setViewport({width:1200,height:800});
+
+    await page.goto('http://www.hyundaihmall.com/Home.html',{waitUntil:"domcontentloaded"});
+    await page.once('load',async ()=>{
+      await page.screenshot({path:datapath+"/pc/"+"site.png",fullPage:true});
+      await page.evaluate(()=>{
+        openLoginPopup()
+      })
+
+      var param = {
+        "browser" : browser,
+        "page" : page,
+        "data" : USERINFO,
+        "folder" : date.text,
+        "datapath" : datapath+"/pc/"
+      }
+      logger.debug("INIT :: 로그인을 시작합니다.");
+      await login.run(param, hmall.main);
+    })
+  });
+}
